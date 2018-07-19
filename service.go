bc0a67d795819fc840f29157bcdbd5fe2410e46f
Mike Schladt <michael.schladt@gmail.com>
2018-07-19 19:04:15 -0400

    Removing build breaking references to kardianos repo

--- service.go
+++ service.go
@@ -13,53 +13,8 @@
 //
 // Examples in the example/ folder.
 //
-//	package main
-//
-//	import (
-//		"log"
-//
-//		"github.com/kardianos/service"
-//	)
-//
-//	var logger service.Logger
-//
-//	type program struct{}
-//
-//	func (p *program) Start(s service.Service) error {
-//		// Start should not block. Do the actual work async.
-//		go p.run()
-//		return nil
-//	}
-//	func (p *program) run() {
-//		// Do work here
-//	}
-//	func (p *program) Stop(s service.Service) error {
-//		// Stop should not block. Return with a few seconds.
-//		return nil
-//	}
-//
-//	func main() {
-//		svcConfig := &service.Config{
-//			Name:        "GoServiceTest",
-//			DisplayName: "Go Service Test",
-//			Description: "This is a test Go service.",
-//		}
-//
-//		prg := &program{}
-//		s, err := service.New(prg, svcConfig)
-//		if err != nil {
-//			log.Fatal(err)
-//		}
-//		logger, err = s.Logger(nil)
-//		if err != nil {
-//			log.Fatal(err)
-//		}
-//		err = s.Run()
-//		if err != nil {
-//			logger.Error(err)
-//		}
-//	}
-package service // import "github.com/kardianos/service"
+
+package service
 
 import (
 	"errors"
